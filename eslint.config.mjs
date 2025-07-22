04c9f9741ff4fb6411ae9cd573403b1e72708d14
Sahydi <sahydi@protonmail.com>
2025-07-22 09:00:18 -0300

    add eslint and prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,28 @@
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default [
+  ...tseslint.configs.recommended, // aplica primeiro as regras padrão
+
+  {
+    files: ['**/*.{js,ts}'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: process.cwd(),
+      },
+      globals: {
+        ...globals.node,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+    },
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'error',
+      'no-console': 'off',
+    },
+  },
+];
