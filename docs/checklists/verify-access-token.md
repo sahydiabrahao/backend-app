c844b2f19d594166a25b428e063bc0375e5ec8b6
Sahydi <sahydi@protonmail.com>
2025-07-30 09:10:11 -0300

    feat(verify-access-token.jwt-adapter): should return payload on success

--- docs/checklists/verify-access-token.md
+++ docs/checklists/verify-access-token.md
@@ -6,16 +6,16 @@
 
 ## ⚙️ APPLICATION
 
-- [ ] `verify-access-token.use-case.ts`
+- [x] `verify-access-token.use-case.ts`
 - [x] Test: should call VerifyAccessToken with correct input
 - [x] Test: should throw if VerifyAccessToken throws
 - [x] Test: should return payload on success
 
 ## 🛠️ INFRA (JWT)
 
-- [ ] `verify-access-token.jwt-adapter.ts`
+- [x] `verify-access-token.jwt-adapter.ts`
 - [x] Test: should call jwt.verify with correct input
 - [x] Test: should throw if jwt.verify throws
 - [x] Test: should throw if token is invalid
 - [x] Test: should throw if token is expired
-- [ ] Test: should return payload on success
+- [x] Test: should return payload on success
--- src/tests/infra/verify-access-token/verify-access-token.jwt-adapter.spec.ts
+++ src/tests/infra/verify-access-token/verify-access-token.jwt-adapter.spec.ts
@@ -14,38 +14,38 @@ const makeSut = () => {
 describe('VerifyAccessTokenJwtAdapter', () => {
   it('should call jwt.verify with correct input', async () => {
     const { sut } = makeSut();
-
     const token = jwt.sign(PAYLOAD, SECRET);
     const verifySpy = jest.spyOn(jwt, 'verify');
     await sut.verify({ token });
-
     expect(verifySpy).toHaveBeenCalledWith(token, SECRET);
   });
 
   it('should throw if jwt.verify throws', async () => {
     const { sut } = makeSut();
-
     jest.spyOn(jwt, 'verify').mockImplementationOnce(() => {
       throw new Error('jwt internal failure');
     });
-
     await expect(sut.verify(VALID_TOKEN)).rejects.toThrow('jwt internal failure');
   });
 
   it('should throws if token is invalid (wrong secret)', async () => {
     const { sut } = makeSut();
-
     const token = jwt.sign(PAYLOAD, INVALID_SECRET);
-
     await expect(sut.verify({ token })).rejects.toThrow('invalid signature');
   });
   it('should return null if token is expired', async () => {
     const { sut } = makeSut();
-
     const expiredToken = jwt.sign(PAYLOAD, SECRET, {
       expiresIn: -1,
     });
-
     await expect(sut.verify({ token: expiredToken })).rejects.toThrow('jwt expired');
+  });
+
+  it('should return payload on success', async () => {
+    const { sut } = makeSut();
+    const token = jwt.sign(PAYLOAD, SECRET);
+    const result = await sut.verify({ token });
+
+    expect(result).toEqual({ userId: PAYLOAD.sub });
   });
 });
